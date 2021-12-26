ab15146a5c9a4445ac70f7440f64aca186ca9267
KoaraCom <eetsutto@gmail.com>
2021-12-26 13:52:34 +0900

    flutter_ddd 練習 https://kabochapo.hateblo.jp/entry/2019/11/01/195130

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// import 'package:practice_flutter_ddd/application/category_app_service.dart';
+// import 'package:practice_flutter_ddd/infrastructure/category/category_factory.dart';
+// import 'package:practice_flutter_ddd/infrastructure/category/category_repository.dart';
+// import 'package:practice_flutter_ddd/infrastructure/db_helper.dart';
+// import 'package:practice_flutter_ddd/infrastructure/note/note_repository.dart';
+import 'package:practice_flutter_ddd/init.dart';
+// import 'package:practice_flutter_ddd/presentation/notifier/category_notifier.dart';
+// import 'package:practice_flutter_ddd/presentation/page/init.dart';
+
+const appTitle = 'Notes';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        // Provider<DbHelper>(
+        //   create: (_) => DbHelper(),
+        //   dispose: (_, helper) async => await helper.dispose(),
+        // ),
+        // Provider<CategoryRepositoryBase>(
+        //   create: (context) => CategoryRepository(
+        //     dbHelper: context.read<DbHelper>(),
+        //   ),
+        // ),
+        // Provider<NoteRepositoryBase>(
+        //   create: (context) => NoteRepository(
+        //     dbHelper: context.read<DbHelper>(),
+        //   ),
+        // ),
+        // ChangeNotifierProvider<CategoryNotifier>(
+        //   create: (context) => CategoryNotifier(
+        //     app: CategoryAppService(
+        //       factory: const CategoryFactory(),
+        //       repository: context.read<CategoryRepositoryBase>(),
+        //       noteRepository: context.read<NoteRepositoryBase>(),
+        //     ),
+        //   ),
+        // ),
+        // Provider<AppInit>(
+        //   create: (context) => AppInit(
+        //     navigatorKey: GlobalKey<NavigatorState>(),
+        //     dbHelper: context.read<DbHelper>(),
+        //   ),
+        // ),
+      ],
+      child: const _Init(),
+    );
+  }
+}
+
+class _Init extends StatelessWidget {
+  const _Init();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(primarySwatch: Colors.green),
+      navigatorKey: Provider.of<AppInit>(context).navigatorKey,
+      onGenerateRoute: (_) {
+        return MaterialPageRoute<dynamic>(
+          builder: (_) => const InitPage(appTitle: appTitle),
+        );
+      },
+    );
+  }
+}
